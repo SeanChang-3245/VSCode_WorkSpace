93b1643e3c92d7af21a22be2c6f88305de6ba135
SeanChang-3245 <70448313+SeanChang-3245@users.noreply.github.com>
2021-12-24 12:20:26 +0800

    12/24

--- Topic/Knapsack/Bounded_knapsack_NM.cpp
+++ Topic/Knapsack/Bounded_knapsack_NM.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long 
+#define ll long long
 #define endl '\n'
 
 int main()
@@ -12,14 +12,38 @@ int main()
     ll n, m;
     cin >> n >> m;
 
-    vector<int> cnt(n), v(n), w(n);
-    vector<vector<int>> dp(n+1, vector<int>(m+1));
+    vector<ll> w(n), v(n), c(n);
+    vector<vector<ll>> dp(n + 1, vector<ll>(m + 1, 0));
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
+        cin >> w[i] >> v[i] >> c[i];
+
+    for (int i = 0; i < n + 1; i++)
     {
-        cin >> w[i] >> v[i] >> cnt[i];
+        for (int x = 0; x < c[i - 1]; x++)
+        {
+            deque<ll> mq;
+
+            for (int k = x; k < m + 1; k += w[i - 1])
+            {  
+                while (mq.size())
+                {
+                    if (dp[i - 1][k] > dp[i - 1][mq.back()] + (k - mq.back()) / w[i - 1] * v[i - 1])
+                        mq.pop_back();
+                    else
+                        break;
+                }
+
+                mq.push_back(k);
+
+                if ((k - mq.front()) / w[i - 1] > c[i - 1])
+                    mq.pop_front();
+
+                dp[i][k] = dp[i - 1][mq.front()] + (k - mq.front()) / w[i - 1] * v[i - 1];
+            }
+        }
     }
 
-    
-
+    cout << dp[n][m];
+    return 0;
 }
